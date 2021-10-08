4ecbc68e0f2a5e297b6af1a895d3a4a272ae50ae
Луцевич Даня <luchevich31@gmail.com>
2021-10-08 18:59:23 +0300

    cinFromConsole

--- 3/3.cpp
+++ 3/3.cpp
@@ -7,18 +7,29 @@ class Fraction {
 public:
 
 	std::string fraction;
-	
+
 	void inverse() {
 
 		fraction = fraction.substr(fraction.find("/") + 1, fraction.length()) + "/" + std::to_string(stoi(fraction));
 		
 	}
+
 	void print() {
 
 		std::cout << fraction;
 
 	}
-	
+
+	void cinFromConsole() {
+
+		do {
+
+			std::cin >> fraction;
+
+		} while (stoi(fraction.substr(fraction.find("/") + 1, fraction.length()))==0);
+
+
+	}
 
 };
 
@@ -26,7 +37,7 @@ int main()
 {
 
 	Fraction f;
-	std::cin>>f.fraction;
+	f.cinFromConsole();
 
 	f.inverse();
 	f.print();
