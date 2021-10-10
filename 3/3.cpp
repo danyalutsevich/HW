be6b58dc0da693ada0ea23d19921d40b751e7684
Луцевич Даня <luchevich31@gmail.com>
2021-10-10 20:53:31 +0300

    copy constructor, copy operator

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,136 +1,64 @@
+﻿// CopyMove.cpp : Copy & Move semantics
+//
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-class Fraction {
-
+class Type {
 public:
+    int x;
+    int arraySize;
+    char* str;
 
-	int numerator;
-	int denominator;
-	
-	Fraction() {
-
-
-
-	}
-	Fraction(int numerator, int denominator) {
-
-		this->numerator = numerator;
-		this->denominator = denominator;
-
-	}
-
-	void print() {
-
-		std::cout << numerator << "/" << denominator;
-
-	}
-	void inverse() {
-
-		int temp = numerator;
-		numerator = denominator;
-		denominator = temp;
-
-	}
-	void cinFromConsole() {
-
-		std::cin >> numerator;
-		do {
-
-			std::cin >> denominator;
-
-		} while (!denominator);
+    Type() {
 
 
+    }
+    Type(int x):Type() {
 
-	}
-	void operator +=(Fraction temp) {
+        this->x = x;
 
-		this->numerator *= temp.denominator;
-		temp.numerator *= this->denominator;
-		this->numerator += temp.numerator;
+    }
+    Type(int x,int arraySize):Type(x) {
 
-		this->denominator *= temp.denominator;
-		temp.denominator = this->denominator;
+        this->arraySize = arraySize;
+        this->str = new char[arraySize];
 
+    }
+    Type(const Type &temp) {
 
-	}
+        this->x = temp.x;
+        this->arraySize = temp.arraySize;
+        this->str = new char[temp.arraySize];
 
-	Fraction operator +(Fraction temp) {
+    }
+    void operator =(Type temp) {
 
-		Fraction temp2;
-		if (this->denominator==temp.denominator) {
+        this->x = temp.x;
+        this->arraySize = temp.arraySize;
+        this->str = new char[temp.arraySize];
 
-			temp2.denominator = this->denominator;
-			temp2.numerator = this->numerator + temp.numerator;
-
-		}
-		else {
-
-			temp2.numerator = this->numerator * temp.denominator + temp.numerator * this->denominator;
-			temp2.denominator = this->denominator * temp.denominator;
-
-		}
-
-		return temp2;
-
-	}
-	Fraction operator +(int temp) {
-
-		Fraction temp2;
-
-		temp2.numerator = this->numerator  + this->denominator * temp;
-
-		temp2.denominator = this->denominator;
-
-		return temp2;
-
-	}
-
-	Fraction operator -(Fraction temp) {
-
-		Fraction temp2;
-
-		if (this->denominator == temp.denominator) {
-
-			temp2.denominator = this->denominator;
-			temp2.numerator = this->numerator - temp.numerator;
-
-		}
-		else {
-
-			temp2.numerator = this->numerator * temp.denominator - temp.numerator * this->denominator;
-			temp2.denominator = this->denominator * temp.denominator;
-
-		}
-
-		return temp2;
-
-	}
-
-	Fraction operator -(int temp) {
-
-		Fraction temp2;
-
-		temp2.numerator = this->numerator - this->denominator * temp;
-
-		temp2.denominator = this->denominator;
-
-		return temp2;
-
-	}
-
-
+    }
 };
 
 int main()
 {
+    Type A;
+    A.x = 10;
+    A.str = new char[100];  // Перенести в конструктор
+    strcpy(A.str, "Hello World!\n");
 
-	Fraction f(3, 4);
-	Fraction d(1, 4);
-	f = f - d;
 
-	//f.cinFromConsole();
-	//f.inverse();
-	f.print();
+    // Создать конструктор копирования
+    Type B = A;  // CopyCtr(-), Copy=(-), DefCtr, Def=
+    B.str[2] = 'X';
 
+    // Создать оператор копирования
+    Type C;
+    C = A;  // Copy=(-), Def=
+
+    std::cout << (void*)A.str << " " << A.str;
+    std::cout << (void*)B.str << " " << B.str;
+    std::cout << (void*)C.str << " " << C.str;
+
+    return 0;
 }
